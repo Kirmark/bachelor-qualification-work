a91420bfceb8628744621c4b1953220769eb6237
”MarkinK” <markinkirill@gmail.com>
2019-05-25 15:44:58 +0300

    изменен адрес для батчей

--- application/data_clean/3_txt_vw_to_batches.py
+++ application/data_clean/3_txt_vw_to_batches.py
@@ -1,7 +1,7 @@
 import artm
 
 FILE_ADRESS = 'application/data_clean/test.txt'
-
+BATCH_ADRESS = 'application/data_clean'
 
 #print(artm.version())
 
@@ -12,7 +12,7 @@ FILE_ADRESS = 'application/data_clean/test.txt'
 batch_vectorizer = artm.BatchVectorizer(
     data_path=FILE_ADRESS, 
     data_format="vowpal_wabbit", 
-    target_folder="test",
+    target_folder=BATCH_ADRESS,
     batch_size=100,
 )
 
