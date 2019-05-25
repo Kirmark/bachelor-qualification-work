aeb829b783d3beea0454a40c55a6deaf28d1236c
”MarkinK” <markinkirill@gmail.com>
2019-05-25 15:32:10 +0300

    добавлен набросок преобразования в батчи

--- application/data_clean/3_txt_vw_to_batches.py
+++ application/data_clean/3_txt_vw_to_batches.py
@@ -2,19 +2,25 @@ import artm
 
 FILE_ADRESS = 'application/data_clean/test.txt'
 
+
+#print(artm.version())
+
+
+
 #model = artm.ARTM()
-'''
+
 batch_vectorizer = artm.BatchVectorizer(
     data_path=FILE_ADRESS, 
     data_format="vowpal_wabbit", 
     target_folder="test",
     batch_size=100,
 )
-'''
 
+'''
 batch_vectorizer = artm.BatchVectorizer(
     data_path=FILE_ADRESS,
     data_format='vowpal_wabbit',
     collection_name='test',
     target_folder='test'
 )
+'''
