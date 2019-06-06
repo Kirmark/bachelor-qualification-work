724a339dc151398ead20e54ab58df08851b5bb2e
”MarkinK” <markinkirill@gmail.com>
2019-06-06 16:40:04 +0300

    сильно ускорен процесс очистки данных

--- application/data_clean/1_base_text_to_base_vw.py
+++ application/data_clean/1_base_text_to_base_vw.py
@@ -4,10 +4,13 @@
 import sqlite3
 import datetime
 import multiprocessing as mp
+import re
+import time
+import random as rnd
+# Библиотека для леммирования
+from pymystem3 import Mystem
 
-import libs.my_vw_lib as vw
-
-WORKER_NUM = 16
+WORKER_NUM = 4
 
 #MODE = 'body_text'
 MODE = 'html'
@@ -16,10 +19,13 @@ MODE = 'html'
 #DB_ADRESS = 'application/data_raw/spider.sqlite'
 DB_ADRESS = 'spider.sqlite'
 # Количество записей обрабатываемых за один проход
-PART_LIMIT = 50
+PART_LIMIT = 400
 
 def clean_one_part(i):
-    	
+	
+	# Для леммирования
+	mystem = Mystem()
+	
 	# Соединение с БД
 	conn = sqlite3.connect(DB_ADRESS)
 	cur = conn.cursor()
@@ -28,28 +34,33 @@ def clean_one_part(i):
 	# или пока не закончатся необработанные даныные в базе
 	data = [0]
 	while len(data) != 0:
+		try:
+			# Вывод текущей ситуации обработки данных в базе
+			#print()
+			cur.execute("SELECT count() FROM Pages")
+			print("Процесс_{0:0>3}: ".format(num) + '{0:30}'.format("Записей в базе:"),
+						'{0:7}'.format(cur.fetchone()[0]))
+			cur.execute("SELECT count() FROM Pages WHERE vowpal_wabbit_date is not NULL")
+			print("Процесс_{0:0>3}: ".format(num) + '{0:30}'.format("Обработано записей:"),
+						'{0:7}'.format(cur.fetchone()[0]))
+			#print()
 
-		# Вывод текущей ситуации обработки данных в базе
-		#print()
-		cur.execute("SELECT count() FROM Pages")
-		print("Процесс_{0:0>3}: ".format(num) + '{0:30}'.format("Записей в базе:"),
-					'{0:7}'.format(cur.fetchone()[0]))
-		cur.execute("SELECT count() FROM Pages WHERE vowpal_wabbit_date is not NULL")
-		print("Процесс_{0:0>3}: ".format(num) + '{0:30}'.format("Обработано записей:"),
-					'{0:7}'.format(cur.fetchone()[0]))
-		#print()
-
-		# Получение данных из базы по PART_LIMIT штук
-		cur.execute(
-			"SELECT url, "+MODE+", vowpal_wabbit, vowpal_wabbit_date, title_text FROM Pages "
-			+ "WHERE vowpal_wabbit_date is NULL and "+MODE+" is not NULL "
-			# Сделано так, что бы быстрее работал RANDOM()
-			+ "and id IN (SELECT id FROM Pages ORDER BY RANDOM() "
-			+ "LIMIT "
-			+ str(PART_LIMIT)
-			+ ")"
-		)
-		data = cur.fetchall()
+			# Получение данных из базы по PART_LIMIT штук
+			cur.execute(
+				"SELECT url, "+MODE+", vowpal_wabbit, vowpal_wabbit_date, title_text FROM Pages "
+				+ "WHERE vowpal_wabbit_date is NULL and "+MODE+" is not NULL "
+				# Сделано так, что бы быстрее работал RANDOM()
+				+ "and id IN (SELECT id FROM Pages ORDER BY RANDOM() "
+				+ "LIMIT "
+				+ str(PART_LIMIT)
+				+ ")"
+			)
+			data = cur.fetchall()
+		except:
+			print("Процесс_{0:0>3}: ".format(num) + "при чтении база была заблокирована, работа продолжена")
+			data = [0]
+			time.sleep(rnd.uniform(10.0, 20.0))
+			continue
 
 		# Построчная обработка полученных данных
 		for i in range(len(data)):
@@ -68,7 +79,35 @@ def clean_one_part(i):
 			if len(text) > 30:
 
 				# Леммирование текста, переработка в формат vowpal_wabbit
-				vw_text = vw.text_to_vowpal_wabbit(text)
+				
+				s = text
+
+				# Проверка входных параметров и объявление переменных
+				if len(s) < 30:
+					raise Exception("Длинна строки меньше 30 символов")
+				res = ''
+
+				# Добавление модальности - текст
+				res = '|text '
+
+				# Леммирование - получаем массив слов
+				lemmas = mystem.lemmatize(s)
+
+				for l in lemmas:
+					l_strip = l.strip()
+
+					if (
+						# Пропуск слишком коротких слов
+						len(l_strip) > 3
+						# Проверка на наличие русских букв
+						and re.match("^[А-Яа-я]*$", l_strip)
+						# Проверка длинны слова
+						and len(l_strip) < 30
+					):
+						# Пропуск слишком длинных слов
+						res = res + l_strip + ' '
+
+				vw_text = res
 
 				# Запись результатов в столбец vowpal_wabbit
 				data[i][2] = vw_text
@@ -77,21 +116,27 @@ def clean_one_part(i):
 				data[i][3] = int((datetime.datetime.utcnow() -
 									datetime.datetime(1970, 1, 1)).total_seconds())
 			# Периодический вывод id, что бы пользователь понимал, что все ОК
-			if i % 10 == 0:
-				print("Процесс_{0:0>3}: ".format(num) + "подготовлена запись номер", i)
+			#if i % 100 == 0:
+			#	print("Процесс_{0:0>3}: ".format(num) + "подготовлена запись номер", i)
 
-		# Запись результатов в базу
-		for i in range(len(data)):
-			cur.execute("UPDATE Pages SET vowpal_wabbit=?, vowpal_wabbit_date=? WHERE url=?", (
-				data[i][2],		# Столбец: vowpal_wabbit
-				data[i][3],		# Столбец: vowpal_wabbit_date
-				data[i][0],		# Столбец: url
-			))
-			if i % 50 == 0:
+		try:
+			# Запись результатов в базу
+			for i in range(len(data)):
+				cur.execute("UPDATE Pages SET vowpal_wabbit=?, vowpal_wabbit_date=? WHERE url=?", (
+					data[i][2],		# Столбец: vowpal_wabbit
+					data[i][3],		# Столбец: vowpal_wabbit_date
+					data[i][0],		# Столбец: url
+				))
+				#if i % 50 == 0:
 					# Периодический коммит, что бы не потерять
-				# слишком много данных при остановке программы
-				conn.commit()
-		conn.commit()
+					# слишком много данных при остановке программы
+				#	conn.commit()
+			conn.commit()
+		except:
+			print("Процесс_{0:0>3}: ".format(num) + "при вставке база была заблокирована, работа продолжена")
+			data = [0]
+			time.sleep(rnd.uniform(10.0, 20.0))
+			continue
 
 	print("Процесс{0:0>3}: ".format(num) + "Все записи обработаны")
 
@@ -106,6 +151,7 @@ try:
 		mp.Process(target=clean_one_part, args=(
 			num,
 		)).start()
+		time.sleep(rnd.uniform(1.0, 2.0))
 
 except KeyboardInterrupt:	# Ctrl+C
 	print()
