0673b5f09a9ec26a8e0f037ef17613527d1ef306
”MarkinK” <markinkirill@gmail.com>
2019-06-09 14:44:46 +0300

    добавлен функционал с библиографией

--- application/data_clean/3_txt_vw_to_batches.py
+++ application/data_clean/3_txt_vw_to_batches.py
@@ -4,11 +4,14 @@
 import artm
 
 # Адрес файла с исходными данными
-FILE_ADRESS = 'application/data_clean/result_clean/news_in_vowpal_wabbit_ria_1kk.txt'
+FILE_ADDRESS = 'application/data_clean/result_clean/news_in_vowpal_wabbit_ria_1kk.txt'
 # Адрес папки для сохранения результата
-BATCH_ADRESS = 'application/models/batches_news_ria_1kk'
+BATCH_ADDRESS = 'application/models/batches_news_ria_1kk'
 
 # Преобразование в формат для bigartm
 batch_vectorizer = artm.BatchVectorizer(
-    data_path=FILE_ADRESS, data_format='vowpal_wabbit', collection_name='news', target_folder=BATCH_ADRESS)
-
+    data_path=FILE_ADDRESS, 
+    data_format='vowpal_wabbit', 
+    collection_name='news', 
+    target_folder=BATCH_ADDRESS
+)
